275ffa4dac6feb5b86b5cdf32584ca37415ab685
Alphonse Johnson <a.joh26@gmail.com>
2023-03-11 09:36:56 -0600

    Figured out prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passChoices = ["1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*"]
+
+
+
+
+ // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+ 
+  
+
+ 
+  // generatePassword =document.createElement(passChoices);
 
   passwordText.value = password;
 
+  
+
+  var randomNumber = Math.floor(math.random() * passChoices.length)
+ 
 }
+  // var userAnswer = window.prompt("Enter your choice of length of string")
+//  window.alert("Password"); 
+
+//   if(!passChoices){
+//     alert("bad bad!")
+//     return;
+//   } 
+
+//   if(!passchoices.includes(passChoices.includes())){
+//     alert("you win");
+//   }
+// }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passQuest = prompt("please enter your choice");
