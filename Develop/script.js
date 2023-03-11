94222c5aed6078e8fc9fb5c720eefff4d0847fd3
Alphonse Johnson <a.joh26@gmail.com>
2023-03-11 10:07:12 -0600

    Added reasonable variables for input choices

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 var passChoices = ["1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*"]
 
+var minChar = 7;
+var maxCharlimit = 140;
+var numPick = ["1234567890"];
+var alphaPick = ["abcdefghijklmnopqrstuvwxyz"]
+var specialPick = ["!@#$%^&*"];
+var caseUpper = document.body.setAttribute("div", "Uppercase");
+var caseLower = document.body.setAttribute("div", "Lowercase");
+
+
+
 
 
 
  // Write password to the #password input
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
  
-  
+
 
  
-  // generatePassword =document.createElement(passChoices);
+generatePassword = document.createElement(passChoices);
 
-  passwordText.value = password;
+// passwordText.textContent = "This is your result " + password + ".";
 
+  // passwordText.value = password;
+
+  // document.body.appendChild(passwordText);
   
+  // document.querySelector("Your Secure Password")
 
-  var randomNumber = Math.floor(math.random() * passChoices.length)
+
+// var nextQuestion = confirm("how many character do you need")
+
+
+  // var randomNumber = Math.floor(math.random() * passChoices.length)
  
 }
   // var userAnswer = window.prompt("Enter your choice of length of string")
