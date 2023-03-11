99e21544e61b87062bcf5b7442a036ad6b582d2f
Alphonse Johnson <a.joh26@gmail.com>
2023-03-11 13:29:17 -0600

    Added more prompts funcationality for the series of questions

--- Develop/script.js
+++ Develop/script.js
@@ -21,13 +21,54 @@ var passchars = [];
 
 
 function writePassword() {
+
+  var userAnswer = window.prompt("Enter your choice of length of password")
+  // console.log("its working " + userAnswer);
+
+  if (!userAnswer) {
+    return;
+  }
+
+
+  if (userAnswer < minCharLimit || userAnswer > maxCharlimit) {
+    window.alert("please enter a valid character limit between 7 & 140")
+    return;
   
-  var userAnswer = window.prompt("Enter your choice of length of string")
+  } else if
+    (userAnswer => minCharLimit || userAnswer <= maxCharlimit)
+    console.log("we in business now")
+    
+    var alphaQuestion = confirm("Would you like to add numeric values")
+   
+    if(alphaQuestion){
+    console.log("USer wants to add numeric value") 
+    } else 
+    console.log("user does not want to add number values")
+
+    var lowerQuestion =confirm ("Would like the password have lowercase letters")
+
+    if(lowerQuestion){
+      console.log("User wants to add lower case letters")
+    }else
+    console.log("user does not want to add lower case letters")
 
 
-  console.log("its working " + userAnswer);
+    var upperQuestion =confirm ("Would like the password have uppercase letters")
 
-  var response = "Thanks for playing" + generateBtn.value + "weeee" + generateBtn.value;
+    if(upperQuestion){
+      console.log("User wants to add upper case letters")
+    }else
+    console.log("user does not want to add upper case letters")
+
+    var specialQuestion =confirm ("Would like the password have lowercase letters")
+
+    if(specialQuestion){
+      console.log("User wants to add special characters")
+    }else
+    console.log("user does not want to special characters")
+
+
+  var response = userAnswer;
 
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
